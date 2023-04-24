b5fc118483427eb442548d0b72db0973818e4ac9
Angel Zúñiga Navarro <angel.zuna@icloud.com>
2023-04-24 17:20:11 -0500

    more setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,61 +1,73 @@
 "use client";
 import React, { useState } from "react";
+import type { Dispatch } from "react";
 import axios from "axios";
 
 const send = async (Date: string, Hour: string, Amount: string) => {
-  const respuesta = await axios.post("https://6ns3c6-3000.csb.app/purchases", {
-    Date: Date,
-    Hour: Hour,
-    Amount: Amount,
+  const respuesta = await axios.post("http://localhost:4000/purchases", {
+    date: Date,
+    hour: Hour,
+    amountSpent: Amount,
   });
   return respuesta.data;
 };
 
 const obtainPurchase = async () => {
-  const respuesta = await axios.get("https://6ns3c6-3000.csb.app/purchases");
-  return respuesta.data;
+  const respuesta = await axios.get("http://localhost:4000/purchases");
+  console.log(respuesta.data[0]);
+  return respuesta.data[0];
 };
 const test = async () => {
-  const respuesta = await axios.get("https://6ns3c6-3000.csb.app/");
-  console.log(respuesta.data);
+  try {
+    const respuesta = await axios.get("http://localhost:4000/");
+    console.log(respuesta.data[0]);
+    return respuesta.data[0];
+  } catch (error) {
+    console.log(error);
+    return "error";
+  }
 };
 
 interface InputProps {
   placeholder?: string;
+  value?: string;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
 }
-const Input = ({ placeholder }: InputProps) => {
+const Input = ({ placeholder, value, setValue }: InputProps) => {
   return (
     <input
       type="text"
       placeholder={placeholder}
-      className="w-3/4 p-4 rounded-2xl m-4 text-center"
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+      className="w-3/4 p-4 rounded-2xl m-4 text-center text-[#09051a] "
     />
   );
 };
 
-interface ButtonProps {
-  fnc: any;
-  children: string;
-}
-
-const Button = ({ fnc, children }: ButtonProps) => {
-  return (
-    <button
-      className=""
-      onClick={(e) => {
-        e.preventDefault();
-        fnc();
-      }}
-    >
-      {children}
-    </button>
-  );
-};
-
 const Page = () => {
-  const Date = useState<string>("");
-  const Hour = useState<string>("");
-  const Amount = useState<string>("");
+  const [date, setDate] = useState<string>("17:00:00");
+  const [hour, setHour] = useState<string>("24:04:2023");
+  const [amount, setAmount] = useState<string>("$15000");
+  const [response, setResponse] = useState<any>([]);
+  const [waiting, setWaiting] = useState<boolean>(false);
+
+  const Button = ({ fnc, children }) => {
+    return (
+      <button
+        className=""
+        onClick={async (e) => {
+          e.preventDefault();
+          const res = Object.values(await fnc());
+          console.log(res);
+          setResponse(res);
+        }}
+      >
+        {children}
+      </button>
+    );
+  };
+
   return (
     <div className="w-full h-[100vh] flex justify-center items-center">
       <div className="flex-wrap w-2/5 h-5/6 flex justify-center rounded-2xl shadow-white shadow-[0px_0px_5px_1px_#ffffff] ">
@@ -63,13 +75,29 @@ const Page = () => {
           Tests
         </div>
         <form className="h-5/6 w-full display flex flex-col justify-around items-center">
-          <Input placeholder="Date" />
-          <Input placeholder="Hour" />
-          <Input placeholder="Amount" />
-          <Button fnc={send}>Send</Button>
+          <Input setValue={setDate} value={date} placeholder="Date" />
+          <Input setValue={setHour} value={hour} placeholder="Hour" />
+          <Input setValue={setAmount} value={amount} placeholder="Amount" />
+          <button
+            onClick={async () => {
+              const answer = await send(date, hour, amount);
+              console.log(answer);
+            }}
+          >
+            Send
+          </button>
           <Button fnc={test}>Test</Button>
           <Button fnc={obtainPurchase}>Get</Button>
         </form>
+      </div>
+      <div className="flex-wrap w-2/5 h-5/6 flex justify-center flex-col rounded-2xl shadow-white shadow-[0px_0px_5px_1px_#ffffff] ">
+        {response.map((item, i) => {
+          return (
+            <div key={i} className="">
+              {item}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
